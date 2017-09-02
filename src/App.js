8123408fbe992c559b2ecc2c7eeb9d224adbb0d6
woonandez <matthew.allen.fernandez@gmail.com>
2017-09-02 10:52:01 -0700

    add card component into app component

--- src/App.js
+++ src/App.js
@@ -1,32 +1,26 @@
 import React, { Component } from 'react';
+import { Card } from 'react-toolbox/lib/card/';
+import { Button } from 'react-toolbox/lib/button/Button';
 
 // import components
 import InputName from './Components/InputName';
+import InputMessage from './Components/InputMessage';
+import DatePicker from './Components/DatePicker';
 
 
-import { Card, CardMedia, CardTitle, CardText, CardActions } from 'react-toolbox/lib/card/';
-import { Button } from 'react-toolbox/lib/button/Button';
 
-const dummyText = 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.';
 
 class App extends Component {
   render() {
     return (
       <Card style={{width: '350px'}}>
         <InputName />
-        <CardMedia
-          aspectRatio="wide"
-          image="https://placeimg.com/800/450/nature"
-        />
-        <CardTitle
-          title="Title goes here"
-          subtitle="Subtitle here"
-        />
-        <CardText>{dummyText}</CardText>
-        <CardActions>
-          <Button label="Action 1" />
-          <Button label="Action 2" />
-        </CardActions>
+        <InputMessage />
+        <DatePicker />
+
+        <Button />
+        <Button />
+
       </Card>
     );
   }
---
+++ src/Components/InputMessage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class InputMessage extends React.Component {
+  render() {
+    return (
+      <h1>InputMessage</h1>
+    )
+  }
+}
+
+export default InputMessage;
