ea2d934a39d0d5668d806db93f7455933edac777
woonandez <matthew.allen.fernandez@gmail.com>
2017-09-02 10:36:04 -0700

    simplify

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
 import React, { Component } from 'react';
-import Container from './Container/Container';
-import './App.css';
+import { Card, CardMedia, CardTitle, CardText, CardActions } from 'react-toolbox/lib/card/';
+import { Button } from 'react-toolbox/lib/button/Button';
+
+const dummyText = 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.';
 
 class App extends Component {
   render() {
     return (
-      <Container />
+      <Card style={{width: '350px'}}>
+        <CardTitle
+          avatar="https://placeimg.com/80/80/animals"
+          title="Avatar style title"
+          subtitle="Subtitle here"
+        />
+        <CardMedia
+          aspectRatio="wide"
+          image="https://placeimg.com/800/450/nature"
+        />
+        <CardTitle
+          title="Title goes here"
+          subtitle="Subtitle here"
+        />
+        <CardText>{dummyText}</CardText>
+        <CardActions>
+          <Button label="Action 1" />
+          <Button label="Action 2" />
+        </CardActions>
+      </Card>
     );
   }
 }
